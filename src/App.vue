7017680b3845138e80e3818db824d2f413557b8b
Daniel Douek <dadouek@gmail.com>
2020-03-29 19:04:12 +0300

    Add project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <SearchBar @termChange="onTermChanged"></SearchBar>
+    <div class="row">
+    <VideoDetail :video="selectedVideo"></VideoDetail>
+    <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import SearchBar from './components/SearchBar.vue';
+import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
+const API_KEY = <API_KEY>;
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchBar,
+    VideoList,
+    VideoDetail
+  },
+  data(){
+    return {videos: [], selectedVideo: null};
+  },
+  methods:{
+    onVideoSelect(video){
+      this.selectedVideo = video;
+    },
+    onTermChanged(searchTerm){
+      axios.get('https://www.googleapis.com/youtube/v3/search',{
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part:'snippet',
+          q: searchTerm
+        }
+      }).then(response => {
+        console.log(response.data.items)
+        this.videos = response.data.items
+      });
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul class="list-group col-md-4">
+        <VideoListItem 
+        v-for="video in videos" 
+        :video="video" 
+        :key="video.id.videoid"
+        @videoSelect="onVideoSelect">
+        </VideoListItem>
+    </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem'
+export default {
+    name: 'VideoList',
+    components:{
+        VideoListItem
+    },
+    props:{
+        videos: Array
+    },
+    methods:{
+        onVideoSelect(video){
+            this.$emit('videoSelect', video)
+        }
+    }
+}
+</script>
